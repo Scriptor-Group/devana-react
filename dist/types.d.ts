1f6de78d57e3866d996c04adf77c4973cc6811f0
Andy Sant <andy@clavus.io>
2024-04-09 16:09:02 +0200

    fix: not using d.ts declaration for import

---
+++ dist/types.d.ts
@@ -0,0 +1,20 @@
+type LLMMessage = {
+    content: string;
+    role: "user" | "assistant" | "system";
+};
+export type IMessage = {
+    id: string;
+    message: LLMMessage;
+    created: number;
+    conversation_id: string;
+    usage: {
+        prompt_tokens: number;
+        completion_tokens: number;
+        total_tokens: number;
+    };
+};
+export type IIntls = {
+    send: string;
+    placeholder: string;
+};
+export {};
